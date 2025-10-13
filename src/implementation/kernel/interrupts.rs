631ea894cbe04466d7a8dad19c0bba8181e785ca
OriZilb <ori.zilberman1@gmail.com>
2025-10-13 12:45:10 +0300

    bug fix + started compiling with nightly rust

--- src/implementation/kernel/interrupts.rs
+++ src/implementation/kernel/interrupts.rs
@@ -50,34 +50,6 @@ pub fn init_idt() {
 }
 
 pub unsafe fn init_pic() {
-    let mut master = Port::new(0x20u16);
-    let mut master_data = Port::new(0x21u16);
-    let mut slave = Port::new(0xA0u16);
-    let mut slave_data = Port::new(0xA1u16);
-
-    // Start initialization (ICW1)
-    master.write(0x11u8);
-    slave.write(0x11u8);
-
-    // Set vector offsets (ICW2)
-    master_data.write(0x20u8); // Master offset
-    slave_data.write(0x28u8);  // Slave offset
-
-    // Tell master about slave (ICW3)
-    master_data.write(0x04u8); // IRQ2 has slave
-    slave_data.write(0x02u8);  // Slave identity
-
-    // ICW4: 8086 mode
-    master_data.write(0x01u8);
-    slave_data.write(0x01u8);
-
-    // Mask all IRQs (just in case)
-    master_data.write(0x0u8);
-    slave_data.write(0x0u8);
-    
-}
-
-pub unsafe fn init_pic() {
     let mut master_control = Port::new(MASTER_CONTROL);
     let mut master_data = Port::new(MASTER_DATA);
     let mut slave_control = Port::new(SLAVE_CONTROL);
